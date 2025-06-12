7d33c257487efe63fec23bd4ba4d5120c36a2f96
Gabriela Chmelařová <chmelarova.gabik@gmail.com>
2025-06-13 00:01:24 +0200

    Removed unused library

--- app/src/main/java/com/gabchmel/youtubesubscriptions/subscriptionsList/data/YoutubeRepository.kt
+++ app/src/main/java/com/gabchmel/youtubesubscriptions/subscriptionsList/data/YoutubeRepository.kt
@@ -1,13 +1,12 @@
 package com.gabchmel.youtubesubscriptions.subscriptionsList.data
 
-import androidx.paging.PagingData
 import com.gabchmel.youtubesubscriptions.subscriptionsList.data.model.ChannelDetails
 import com.gabchmel.youtubesubscriptions.subscriptionsList.data.model.Subscription
 import com.gabchmel.youtubesubscriptions.auth.model.UserProfile
 import kotlinx.coroutines.flow.Flow
 
 interface YoutubeRepository {
-    fun getPagedSubscriptions(order: String): Flow<PagingData<Subscription>>
+    fun getPagedSubscriptions(order: String): Flow<Subscription>
     suspend fun getChannelDetails(channelId: String): Result<ChannelDetails>
     suspend fun getUserProfile(): Result<UserProfile>
 }
